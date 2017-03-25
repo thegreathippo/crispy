e34b6ff1da8688c48826bc49ec617619672a6861
thegreathippo <thegreathippo@gmail.com>
2017-03-25 00:15:30 -0400

    Added todo.

--- crispy/main.py
+++ crispy/main.py
@@ -1,3 +1,11 @@
+"""TODO:
+    * Root's internal dict (data) uses an OrderedDict; this is because we're relying on the 'images' component for
+    sprites, and if sprite is loaded on an entity before image, it creates an error. To fix this, components should
+    be self-contained; sprite should contain both sprite positional data *and* image-data. Accomplishing this means
+    creating a specialized dict-type -- or an internal sprite data class for the sprite component.
+
+"""
+
 from game import world
 import gui
 
